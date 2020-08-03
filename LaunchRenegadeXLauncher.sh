c14b91f73f202385c05a27174b95ec093a208990
Bas Imth <30594663+ShogunKitsune@users.noreply.github.com>
2020-08-03 21:52:27 +0200

    Fixing issues one step at the time. Same goes for winetricks now being one step at the time!

---
+++ LaunchRenegadeXLauncher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Only exists to make some people's life easier.
+# Requires some extra love to be actually a good product and placing thyself in /local/bin.
+WINEPREFIX="$HOME/RenegadeXWine" wine "$HOME/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/Launcher/Renegade X Launcher.exe"
--- renx-setup-script.sh
+++ renx-setup-script.sh
@@ -3,21 +3,24 @@
 WINEPREFIX="$HOME/RenegadeXWine" WINEARCH=win64
 # assume latest winetricks already installed
 # the -q parameter supresses all the GUI warning dialogs, which there are *A LOT* during this process
-WINEPREFIX="$HOME/RenegadeXWine" winetricks -q corefonts vcrun2008 vcrun2010 xact xact_64 d3dx9_43 d3dx9 msxml3 d3dcompiler_43
+# Although it does supress the GUI, it still can't shut the fuck up.
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q corefonts
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q vcrun2008
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q vcrun2010
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q xact
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q xact_x64
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q msxml3
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q d3dcompiler_43
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q d3dx9
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q d3dx9_43
 # d3dx9 sets WINE to identify as Windows 2003, so switch back to to Windows 7 so that dotnet452 installation works
 WINEPREFIX="$HOME/RenegadeXWine" winetricks -q win7
-WINEPREFIX="$HOME/RenegadeXWine" winetricks -q dotnet452 win7
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q dotnet452
+WINEPREFIX="$HOME/RenegadeXWine" winetricks -q win7
 # Obtain the Renegade_X_Installer.msi from DigitalOcean and install it.
 echo Attempting to download Renegade_X_Installer
-wget https://renxdownloads.nyc3.cdn.digitaloceanspaces.com/Renegade_X_Installer-3f1857a.msi > renegadexinstaller.msi
+wget https://renxdownloads.nyc3.cdn.digitaloceanspaces.com/Renegade_X_Installer-3f1857a.msi
 #Let's warn the user.
 echo Attempting to launch the Renegade X installer, please standby.
-WINEPREFIX="$HOME/RenegadeXWine" msiexec /i renegadexinstaller.msi
-echo Awaiting for user to continue once setup is done.
-echo "waiting for user to hit ENTER"
-while read answer; do
-  if [ -z "$answer" ]; then
-    break
-  fi
-done
-WINEPREFIX="$HOME/RenegadeXWine" wine64 "~/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/LauncherRenegade X Launcher.exe"
+WINEPREFIX="$HOME/RenegadeXWine" msiexec /i Renegade_X_Installer-3f1857a.msi
+WINEPREFIX="$HOME/RenegadeXWine" wine "$HOME/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/Launcher/Renegade X Launcher.exe"
