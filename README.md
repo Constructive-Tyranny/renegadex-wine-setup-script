c5066fa69aa36a242b49e684efea5416a29cf207
Bas-Imth <30594663+Bas-Imth@users.noreply.github.com>
2022-03-21 13:38:17 +0100

    Fully automatic

--- README.md
+++ README.md
@@ -12,25 +12,34 @@ An up to date version of **[WINE](https://wiki.winehq.org/Download)**.
 **[Wget](https://www.gnu.org/software/wget/)** package.  
 *So we can download our requirements*  
 
-**Docker**  
+**[Docker](https://docs.docker.com/engine/install/)**  
 *So we can use the update container to download the game*  
 
-**ACL**  
+**[ACL](https://wiki.archlinux.org/title/Access_Control_Lists)**  
 *So we can set permissions on the WINE Prefix to allow the container to operate*  
 
 *Although we are not sure if this is actually necessary, Mono-Complete is also recommended.*
 
 
 ## Features and usage.
-Although the script has the potential to be fully automatic, I've a general tendency to not do that.  
-Users should be aware of what they're using and doing.  
-The script will create a new wineprefix called: RenegadeXWine.  
-And then proceed with downloading the DLL's necessary to run both the UDK.exe and the launcher.
+~~Although the script has the potential to be fully automatic, I've a general tendency to not do that.~~  
+*(Sorry old Sho, but my hand got forced)*  
 
-### Usage  
+This script will download all the required things, this includes:  
+A) Renegade X Launcher Installer  
+B) Unreal Engine 3 Redistributable  
+C) A WINE prefix with all required DLL's to run the game.   
+*(This is using an older WINE prefix which can still run the game without breaking dependencies.)*  
+<br>
+Optional:  
+D) A docker image which can run Sonny's update tool to download & update the game.  
+
+# Usage  
 Download and execute this script.
 ```shell
 wget https://raw.githubusercontent.com/Constructive-Tyranny/renegadex-wine-setup-script/master/rxWine-setup.sh && chmod +rx rxWine-setup.sh && bash rxWine-setup.sh
 ```  
-Once the script starts, a message from WINE setting up config will appear, afterwards (as you may see in the terminal) it starts installing all dependencies in the wineprefix.
-**Any warning from WINE should be ignored, if a app fails to launch, don't view the report. That's legit how it works here.**
+Once the script starts, a message from WINE setting up config will appear, afterwards (as you may see in the terminal) it starts downloading & installing dependencies.   
+
+----
+## **Any warning from WINE should be ignored, if an app fails to launch, don't view the report. This will cause the setup / execution to fail!**
--- rxWine-setup.sh
+++ rxWine-setup.sh
@@ -6,38 +6,76 @@ mkdir -p $HOME/Games
 cd $HOME/Games
 # Our required downloads.
 # 1. The installer.
-echo "======================"
-echo "Downloading installer"
-echo "======================"
-wget https://renxdownloads.b-cdn.net/Renegade_X_Installer-0.87-release.msi
 
+if [ -f "Renegade_X_Installer-0.87-release.msi" ];
+then
+    echo "Installer found."
+else
+    echo "======================"
+    echo "Downloading installer"
+    echo "======================"
+    wget https://renxdownloads.b-cdn.net/Renegade_X_Installer-0.87-release.msi
+fi
 # 1.1 UE3 Redist package.
-echo "======================"
-echo "Downloading UE3 Redist package."
-echo "======================"
-wget https://renxdownloads.b-cdn.net/UE3Redist.exe
+if [ -f "UE3Redist.exe" ];
+then
+    echo "Installer found."
+else
+    echo "==============================="
+    echo "Downloading UE3 Redist package."
+    echo "==============================="
+    wget https://renxdownloads.b-cdn.net/UE3Redist.exe
+fi
+
 # 2. The ENTIRE WINE PREFIX
-echo "======================"
-echo "Downloading the Wine prefix"
-echo "======================"
-wget http://cdn.ctgamehosts.uk/cdn/share/RenegadeXWine.zip
+if [ -f "RenegadeXWine.zip" ];
+then
+    echo "Installer found."
+elif [ -d "RenegadeXWine" ];
+then
+    echo "Directory is there, so skipping download"
+else
+    echo "============================"
+    echo "Downloading the Wine prefix"
+    echo "============================"
+    wget http://cdn.ctgamehosts.uk/cdn/share/RenegadeXWine.zip
+fi
+
 echo "Unzipping.."
 unzip RenegadeXWine.zip
+echo "========================"
 echo "Removing downloaded zip."
+echo "========================"
 rm RenegadeXWine.zip
 
 # Installing installer..
+echo "========================================================="
 echo "Executing the Renegade X Installer, please go through it."
+echo "========================================================="
 WINEPREFIX="$HOME/Games/RenegadeXWine" msiexec /i Renegade_X_Installer-0.87-release.msi
 rm Renegade_X_Installer-0.87-release.msi
 
+echo "======================================================="
 echo "Executing the UE3 Redist package, please go through it."
+echo "======================================================="
+
 WINEPREFIX="$HOME/Games/RenegadeXWine" wine UE3Redist.exe
 rm UE3Redist.exe
 
 # Gamble part.
 # Uncomment if you want to enable this gamble.
-
-#echo "This is a pure gamble, but works if you have docker & facl installed."
-#setfacl -R -m u:1010:rwx $PWD/RenegadeXWine/
-#docker run -it -v "$PWD/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/":/mounted_volume dockerrxupdater:latest
+while true; do
+    echo "This is a pure gamble, but works if you have docker & ACL installed."
+    read -p "Do you want to proceed? (y/n) " yn
+    
+    case $yn in
+        [yY] ) echo ok, we will proceed;
+        break;;
+        [nN] ) echo exiting...;
+        exit;;
+        * ) echo invalid response;;
+    esac
+    
+done
+setfacl -R -m u:1010:rwx $PWD/RenegadeXWine/
+sudo docker run -it -v "$PWD/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/":/mounted_volume basimth/rx-updater:latest
