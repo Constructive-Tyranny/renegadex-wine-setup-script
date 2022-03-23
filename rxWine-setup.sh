0b7d90523877c96dae68dbfa948a3511f0ec62d3
Bas-Imth <30594663+Bas-Imth@users.noreply.github.com>
2022-03-23 11:35:35 +0100

    Wine 7 appears to work fine.

--- rxWine-setup.sh
+++ rxWine-setup.sh
@@ -16,7 +16,7 @@ else
     echo "======================"
     wget https://renxdownloads.b-cdn.net/Renegade_X_Installer-0.87-release.msi
 fi
-# 1.1 UE3 Redist package.
+# 2. UE3 Redist package.
 if [ -f "UE3Redist.exe" ];
 then
     echo "Installer found."
@@ -27,27 +27,21 @@ else
     wget https://renxdownloads.b-cdn.net/UE3Redist.exe
 fi
 
-# 2. The ENTIRE WINE PREFIX
-if [ -f "RenegadeXWine.zip" ];
-then
-    echo "Installer found."
-elif [ -d "RenegadeXWine" ];
-then
-    echo "Directory is there, so skipping download"
-else
-    echo "============================"
-    echo "Downloading the Wine prefix"
-    echo "============================"
-    wget http://cdn.ctgamehosts.uk/cdn/share/RenegadeXWine.zip
-fi
-
-echo "Unzipping.."
-unzip RenegadeXWine.zip
-echo "========================"
-echo "Removing downloaded zip."
-echo "========================"
-rm RenegadeXWine.zip
-
+WINEPREFIX="$HOME/Games/RenegadeXWine" WINEARCH=win64
+# Assume latest winetricks already installed.
+# The -q parameter supresses all the GUI warning dialogs, which there are *A LOT* during this process.
+# Although it does supress the GUI, it still can't shut the fuck up.
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q corefonts
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q vcrun2008
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q vcrun2010
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q xact
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q xact_x64
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q msxml3
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q d3dcompiler_43
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q d3dx9
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q d3dx9_43
+# d3dx9 sets WINE to identify as Windows 2003, so switch back to Windows 7.
+WINEPREFIX="$HOME/Games/RenegadeXWine" winetricks -q win7
 # Installing installer..
 echo "========================================================="
 echo "Executing the Renegade X Installer, please go through it."
@@ -58,7 +52,6 @@ rm Renegade_X_Installer-0.87-release.msi
 echo "======================================================="
 echo "Executing the UE3 Redist package, please go through it."
 echo "======================================================="
-
 WINEPREFIX="$HOME/Games/RenegadeXWine" wine UE3Redist.exe
 rm UE3Redist.exe
 
@@ -69,13 +62,17 @@ while true; do
     read -p "Do you want to proceed? (y/n) " yn
     
     case $yn in
-        [yY] ) echo ok, we will proceed;
+        [yY] ) echo Ok, we will proceed.;
         break;;
-        [nN] ) echo exiting...;
+        [nN] ) echo Exiting...;
         exit;;
-        * ) echo invalid response;;
+        * ) echo Invalid response.;;
     esac
     
 done
+echo "Setting permissions for the container"
 setfacl -R -m u:1010:rwx $PWD/RenegadeXWine/
+echo "Calling on the container to download the game."
 sudo docker run -it -v "$PWD/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/":/mounted_volume basimth/rx-updater:latest
+echo "Setting user permissions back in order."
+sudo chown -R $USER:USER "$PWD/RenegadeXWine/drive_c/Program Files (x86)/Renegade X/"
